c9afd3379631761e2bfb40a3a3f99a9cf5defd79
thejamilah <jamilahmcw@gmail.com>
2019-04-02 16:57:42 -0700

    add solution without tests

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution here!
+'use strict';
+
+driver = ['Milo', 'Otis', 'Garfield']
+
+//push adds to the the end of an array
+const append = driver.push('Odie')
+
+//pop removes from the end of an array
+const prepend = driver.pop('Oddie')
+
+//shift removes from the front of an array
+const removeLast = driver.shift('')
+
+//add to the front of an array 
+const romoveFirst = driver.unshift('')
